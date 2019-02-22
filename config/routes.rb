5fca1b696d8a1943d31313a773e3c4361a6fa79d
Doris <DorisMac@MikeMacBookPro.local>
2019-02-22 15:30:36 +0100

    Added LandingController and welcome/signin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'landing/welcome'
+  get 'landing/signin'
+
+
+
+
+  root 'landing#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
